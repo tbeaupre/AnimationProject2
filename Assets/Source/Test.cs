8aa769b633b2a1ae8323bcf099699b6d2d2a3c40
Tyler Beaupre <tylerbeaupre4@gmail.com>
2017-04-07 10:46:40 -0400

    Added Objects for testing transformations

---
+++ Assets/Source/Test.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+	Vector3 translate = new Vector3(3, 0, 0);
+	Vector3 rotate = new Vector3(0, 30, 0);
+
+	// Use this for initialization
+	void Start () {
+		transform.position = new Vector3(0, 0, 0);
+		transform.eulerAngles = new Vector3(0, 0, 0);
+
+		transform.Translate(translate);
+		transform.Rotate(rotate);
+
+		transform.Translate(translate);
+		transform.Rotate(rotate);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Source/Test2.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test2 : MonoBehaviour {
+	Vector3 translate = new Vector3(3, 0, 0);
+	Vector3 rotate = new Vector3(0, 30, 0);
+
+	// Use this for initialization
+	void Start () {
+		transform.position = new Vector3(0, 0, 0);
+		transform.eulerAngles = new Vector3(0, 0, 0);
+
+		transform.Translate(translate, Space.World);
+		transform.Rotate(rotate, Space.World);
+
+		transform.Translate(translate, Space.World);
+		transform.Rotate(rotate, Space.World);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
